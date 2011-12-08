a13bc2251c1369970525012c37758b738622ab1a
Ed Schneeflock <ed@schneeflock.com>
2011-12-07 23:32:42 -0500

    Switch to using RPN for the input string.

--- erlang/mathtree.erl
+++ erlang/mathtree.erl
@@ -3,14 +3,18 @@
 -export([run/1, parse/1]).
 
 parse(Expression) ->
-    io:fwrite("~p~n", [Expression]),
-    Tokens = string:tokens(Expression, ", "),
-    io:fwrite("~p~n", [Tokens]),
-    Term = make_terms(Tokens, []),
-    io:fwrite("~w~n", [Term]),
-    run(Term).
+    %% get a list of white space sperated tokens
+    Tokens = string:tokens(Expression, " "),
+    Expressions = make_terms(Tokens, []),
+    io:fwrite("~w~n", [Expressions]),
+    run(Expressions).
 
-make_terms([], Out) -> make_expr(lists:reverse(Out));
+%%
+%% make_terms/2
+%% build a list of terms from the list of tokens
+%% finally turn those terms into expressions
+%%
+make_terms([], Out) -> make_expr(lists:reverse(Out), []);
 make_terms(["+"|T], Out) -> make_terms(T, [add|Out]);
 make_terms(["-"|T], Out) -> make_terms(T, [subtract|Out]);
 make_terms(["*"|T], Out) -> make_terms(T, [multiple|Out]);
@@ -18,20 +22,24 @@ make_terms(["/"|T], Out) -> make_terms(T, [divide|Out]);
 make_terms([Number|T], Out) -> make_terms(T, [list_to_integer(Number)|Out]).
 
 
-make_expr([Op, Op1, Op2|Terms]) when is_atom(Op), is_number(Op1), is_number(Op2) ->
-    io:fwrite("3 ~w ~w~n", [Op, Terms]),
-    {Op, Op1, Op2};
-make_expr([Op, Op1, Op2|Terms]) when is_atom(Op), is_number(Op1) ->
-    io:fwrite("2 ~w ~w~n", [Op, [Op2|Terms]]),
-    {Op, Op1, make_expr([Op2|Terms])};
-make_expr([Op, Op1, Op2|Terms]) when is_atom(Op) ->
-    Expr =  make_expr([Op1, Op2|Terms]),
-    io:fwrite("1 ~w ~w ~w~n", [Op, [Op1,Op2|Terms], Expr]),
-    {Op, Expr, make_expr(lists:sublist(Terms, tuple_size(Expr), 50))};
-make_expr([Number|Terms]) ->
-    Number.
+%%
+%% make_expr/2
+%% terms are either a number or an atom denoting an operation
+%% create nested expressions in the form of
+%% {operator, operand-1, operand-2}
+%%
+make_expr([Term|Terms], Numbers) when is_number(Term) ->
+    make_expr(Terms, [Term|Numbers]);
+make_expr([Term|Terms], [N1, N2|Numbers]) -> %% it is an atom
+    case Terms of
+	[] -> {Term, N2, N1};
+	_ ->
+	    make_expr(Terms, [{Term, N2, N1}|Numbers])
+	end.
 
-
+%%
+%% calculate the expression
+%%
 run(N) when is_number(N) ->
     N;
 run({add, Op1, Op2}) ->
